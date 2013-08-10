2612d8d5dea092afd598390f7eb88e6c7786fd0f
Dan <1596016+dan-v@users.noreply.github.com>
2013-08-09 23:52:33 -0400

    Create change_tor_identity.py

---
+++ change_tor_identity.py
@@ -0,0 +1,11 @@
+import socket,sys
+
+try:
+    tor_ctrl = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    tor_ctrl.connect(("127.0.0.1", 9151))
+    tor_ctrl.send('AUTHENTICATE "{}"\r\nSIGNAL NEWNYM\r\n'.format("passwd"))
+    response = tor_ctrl.recv(1024)
+    if response != '250 OK\r\n250 OK\r\n':
+        sys.stderr.write('Unexpected response from Tor control port: {}\n'.format(response))
+except Exception, e:
+    sys.stderr.write('Error connecting to Tor control port: {}\n'.format(repr(e)))
